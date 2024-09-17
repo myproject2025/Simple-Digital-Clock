4e0b769bd2b13184e309330a852b9eab55beeff3
myproject2025 <167327944+myproject2025@users.noreply.github.com>
2024-09-17 14:21:31 +0500

    UPDATE

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 var clock = document.getElementById('clock');
-// function update the clock
 function updateClock() {
     var now = new Date();
-    var hours = String(now.getHours()).padStart(2, '0');
+    var houres = String(now.getHours()).padStart(2, '0');
     var minutes = String(now.getMinutes()).padStart(2, '0');
     var seconds = String(now.getSeconds()).padStart(2, '0');
-    var currentTime = "".concat(hours, ":").concat(minutes, ":").concat(seconds);
+    var currentTime = "".concat(houres, ":").concat(minutes, ":").concat(seconds);
     if (clock) {
         clock.textContent = currentTime;
     }
--- index.ts
+++ index.ts
@@ -1,24 +1,21 @@
 const clock = document.getElementById('clock')
 
 
-// function update the clock
-
 function updateClock() {
     const now = new Date()
-    const hours = String(now.getHours()).padStart(2,'0');
-    const minutes = String(now.getMinutes()).padStart(2,'0');
-    const seconds = String(now.getSeconds()).padStart(2,'0');
+    const houres = String(now.getHours()).padStart(2, '0')
+    const minutes = String(now.getMinutes()).padStart(2, '0')
+    const seconds = String(now.getSeconds()).padStart(2, '0')
 
 
-    const currentTime = `${hours}:${minutes}:${seconds}`
+
+    const currentTime = `${houres}:${minutes}:${seconds}`
 
 
     if (clock) {
         clock.textContent = currentTime
     }
 }
-
-
 
 setInterval(updateClock, 1000)
 
